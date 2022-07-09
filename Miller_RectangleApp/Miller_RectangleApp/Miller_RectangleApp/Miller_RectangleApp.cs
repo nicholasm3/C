b9b5ebbbf9b71cddbfb50178379a54da02ed714b
Nicholas Miller <nicholasm3@yahoo.com>
2022-07-09 12:34:01 -0500

    Add files via upload

---
+++ Miller_RectangleApp/Miller_RectangleApp/Miller_RectangleApp/Miller_RectangleApp.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miller_RectangleApp
+{
+    class Miller_RectangleApp
+    {
+
+
+        //Main method
+        static void Main()
+        {
+
+            WriteLine("Name: Nicholas Miller");
+            WriteLine("Course: ITDEV-115");
+            WriteLine("Instructor: J Christie");
+            WriteLine("Assignment: Rectangle Assignment");
+            WriteLine("Date: 08/26/2021\n");
+            WriteLine("******************************************************************\n");
+            WriteLine("This program will calculate the area and the perimiter of a rectangle.");
+            WriteLine("All you need to do is enter the rectangle's height and width.\n");
+            
+
+            //Variables
+            double height;
+            double width;
+            double area;
+            double perimeter;
+
+            //Values
+            ReadValues(out height, out width);
+
+            //Calculating area
+            area = CalcArea(height, width);
+
+            //Calculating perimeter
+            perimeter = CalcPerimeter(height, width);
+
+            //Display results
+            printResults(height, width, area, perimeter);
+            ReadKey();
+
+
+        }
+
+        //Method that read's the values from user
+        static void ReadValues(out double height, out double width)
+        {
+
+            //Reads height
+            Write("Please enter the rectangles height in inches: ");
+            height = double.Parse(Console.ReadLine());
+
+            //Reads width
+            Write("Please enter the rectangles width in inches: ");
+            width = double.Parse(Console.ReadLine());
+        }
+
+        //Method that calculates area of rectangle
+        static double CalcArea(double height, double width)
+        {
+            return (height * width);
+        }
+
+        //Method that calculates perimeter of rectangle
+        static double CalcPerimeter(double height, double width)
+        {
+            return (2 * (height + width));
+        }
+
+        //Method that prints the results
+        static void printResults(double height, double width, double area, double perimeter)
+        {
+            WriteLine("\n{0, -10}: {1,10:F1}", "The area of the rectangle is", area + " inches");
+            WriteLine("{0, -10}: {1,10:F1}", "The perimeter of the rectangle is", perimeter + " inches");
+        }
+    }
+}
