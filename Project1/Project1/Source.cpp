e600924286b0a15b966109b6216dca79dbb10526
Nicholas Miller <nicholasm3@yahoo.com>
2022-07-09 12:37:06 -0500

    Add files via upload

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+	int height;
+	cout << "How tall is the room? ";
+	cin >> height;
+
+
+	cout << "The height is" << height;
+	
+	return 0;
+}
